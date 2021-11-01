383a38e736ae4c231f09460f1fc654eb07f94663
DybeKk <matdybowski@gmail.com>
2021-11-01 11:34:06 +0100

    added footer responsive

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,9 +5,9 @@ import {AiFillFacebook, AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
 export function Footer() {
     return (
         <div className={classNames(layoutStyles.footer, layoutStyles.container, "flex justify-center items-center")}>
-            <div className="w-1/2">
-                <h2 style={{color: '#faa9bd'}} className="font-bold text-right">@Mateusz Dybowski</h2>
-                <h2 className="textBorder-short font-bold text-white text-2xl">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</h2>
+            <div className="w-3/4 md:w-1/2">
+                <h2 style={{color: '#faa9bd'}} className="font-bold sm:text-sm text-right">@Mateusz Dybowski</h2>
+                <h2 className="textBorder-short text-sm sm:text-2xl md:text-3xl font-bold text-white lg:text-2xl">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</h2>
                 <div className="flex mt-5">
                     <AiFillFacebook size="25" color="white" />
                     <AiFillLinkedin size="25" className="mx-2"  color="white" />
